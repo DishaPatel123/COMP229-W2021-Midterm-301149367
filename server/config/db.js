aa7cd194ac5175ffc73d8af1b5abd209a10d7257
DishaPatel123 <78120625+DishaPatel123@users.noreply.github.com>
2021-03-05 14:25:29 -0500

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+//Disha Patel - 301149367
+//Midterm Exam
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI":"mongodb+srv://dbDisha:pQXlKKk38SxzBpq0@dishapatel.whc8a.mongodb.net/books229?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,18 @@
+//Disha Patel - 301149367
+//Midterm Exam
+
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
